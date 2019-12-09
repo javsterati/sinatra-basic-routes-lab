3b03e4d323509b1bb73dd992602091d092996265
Javi Huerta <javsteratii@gmail.com>
2019-12-09 14:58:46 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is MY NAME IS JAVI, YOU PEASANT."
+      end
+
+      get '/hometown' do
+        "My hometown is song is YA BOI FROM WOODSTOCK, LAND OF CONSERVATIVE PEASANTS."
+      end
+
+      get '/favorite-song' do
+        "My favorite song is Currently, I Would have to say Hope by XXXTENTACION, or maybe something by Megan the Stallion.\n Lets just say trap music in general is my fav song. thanks!"
+      end
+
 end
